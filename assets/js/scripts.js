84898d24c980bb6778634a794d31e06775c6c4ee
igregorioneto <igregoriodoneto@outlook.com>
2021-08-28 11:08:40 -0300

    Ajustando menu hamburguer e chamando menu lateral

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,9 +2,12 @@ let openMenu = false
 
 let clickMenu = document.querySelector('.btn-menu')
 let menuNormal = document.querySelector('.menu-normal')
+let rpgIMG = document.querySelector('.rpg-img')
+let removerMargemBTN = document.querySelector('.btn-menu')
 
 window.addEventListener('load', () => {
   menuNormal.classList.add('esconder-menu')
+  removerMargemBTN.classList.add('btn-menu-margem')
 })
 
 clickMenu.addEventListener('click', () => {
@@ -13,11 +16,21 @@ clickMenu.addEventListener('click', () => {
     menuNormal.classList.remove('esconder-menu')
     menuNormal.classList.add('normalizar-menu')
 
+    rpgIMG.classList.remove('normalizar-imagem')
+    rpgIMG.classList.add('esconder-imagem')
+
+    removerMargemBTN.classList.remove('btn-menu-margem')
+
     openMenu = true
   } else {
     clickMenu.classList.remove('open')
     menuNormal.classList.remove('normalizar-menu')
     menuNormal.classList.add('esconder-menu')
+
+    rpgIMG.classList.remove('esconder-imagem')
+    rpgIMG.classList.add('normalizar-imagem')
+
+    removerMargemBTN.classList.add('btn-menu-margem')
 
     openMenu = false
   }
